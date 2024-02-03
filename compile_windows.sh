5aa782b7ad04b8150b0a269209f5d8f3cc4f14a5
Woronko Andrzej <andrzej.woronko@exorigo-upos.pl>
2024-02-03 01:21:44 +0100

    Poprawki skryptów kompilacji

--- compile_windows.sh
+++ compile_windows.sh
@@ -1,15 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 export PATH=$PATH:"./x86-windows/debug/lib:./x86-windows/debug/bin:./win32_build/Debug:./x86-windows/lib:./x86-windows/bin:./win32_build/Release"
 
-echo "Set path to cmake !!!"
-
 CMAKE=/c/Qt/Tools/CMake_64/bin/cmake.exe
+
+echo "Set path to cmake !!! ${CMAKE}"
 
 rm -rf win32_build
 
 echo "compile Debug Windows 32"
-${CMAKE} -DCMAKE_BUILD_TYPE=Debug -S ./  -A Win32 -B win32_build ; ${CMAKE} --build win32_build --config Debug
+${CMAKE} -DCMAKE_BUILD_TYPE=Debug -S ./  -B win32_build ; ${CMAKE} --build win32_build --config Debug
 
 echo "compile Release Windows 32"
-${CMAKE} -DCMAKE_BUILD_TYPE=Release -S ./ -A Win32 -B win32_build ; ${CMAKE} --build win32_build --config Release
+${CMAKE} -DCMAKE_BUILD_TYPE=Release -S ./ -B win32_build ; ${CMAKE} --build win32_build --config Release
